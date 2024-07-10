35230cd959a777f82bb1837d30cc3e332dbdf984
xAdiro <adiro.rostek@gmail.com>
2024-07-10 02:01:09 +0200

    Injected StudentRepository for first method in LessonController

--- src/main/java/pl/adiro/langlionapi/controller/LessonController.java
+++ src/main/java/pl/adiro/langlionapi/controller/LessonController.java
@@ -1,9 +1,15 @@
 package pl.adiro.langlionapi.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.adiro.langlionapi.model.lessons.Student;
+import pl.adiro.langlionapi.repository.StudentRepository;
 
 @RestController
 public class LessonController {
 
-
+    @GetMapping("/students")
+    public Iterable<Student> getStudents(StudentRepository studentRepository){
+        return studentRepository.findAll();
+    }
 }
