ebe6dae29fd606f8fc6603ad1d5f76195d2ef224
xAdiro <adiro.rostek@gmail.com>
2024-07-29 03:15:07 +0200

    Got rid of repository classes that don't connect to databases

--- src/main/java/pl/adiro/langlionapi/controller/LessonController.java
+++ src/main/java/pl/adiro/langlionapi/controller/LessonController.java
@@ -3,13 +3,12 @@ package pl.adiro.langlionapi.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.adiro.langlionapi.model.lessons.Student;
-import pl.adiro.langlionapi.repository.StudentRepository;
 
 @RestController
 public class LessonController {
 
     @GetMapping("/students")
-    public Iterable<Student> getStudents(StudentRepository studentRepository){
-        return studentRepository.findAll();
+    public Iterable<Student> getStudents(){
+        return null;
     }
 }
--- src/main/java/pl/adiro/langlionapi/repository/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.adiro.langlionapi.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import pl.adiro.langlionapi.model.lessons.Student;
-
-@Repository
-public interface StudentRepository extends CrudRepository<Student, String> {
-}
