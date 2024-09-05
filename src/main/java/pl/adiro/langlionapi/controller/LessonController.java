f8ef5cfe46c9d5bbaa26260a83ad46a7e585af2e
xAdiro <adiro.rostek@gmail.com>
2024-09-05 04:00:05 +0200

    Made LessonController use LessonService

--- src/main/java/pl/adiro/langlionapi/controller/LessonController.java
+++ src/main/java/pl/adiro/langlionapi/controller/LessonController.java
@@ -1,11 +1,22 @@
 package pl.adiro.langlionapi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.adiro.langlionapi.model.lessons.Student;
+import pl.adiro.langlionapi.service.LessonService;
 
 @RestController
 public class LessonController {
+
+    private LessonService lessonService;
+
+    public LessonController(){}
+
+    @Autowired
+    public LessonController(LessonService lessonService){
+        this.lessonService = lessonService;
+    }
 
     @GetMapping("/students")
     public Iterable<Student> getStudents(){
